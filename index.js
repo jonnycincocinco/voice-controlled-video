a8de2dd12e3b7f5971aeedeb09585eca94032a4f
Jonathan Larson <jlarson@lendingclub.com>
2020-01-01 15:47:19 -0800

    add files

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/src')); // html
+// app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 2020, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
---
+++ src/js/script.js
@@ -0,0 +1,182 @@
+(function() {
+  'use strict';
+
+  // check for SpeechRecognition API
+  if (!('webkitSpeechRecognition' in window)) {
+    console.log('Speech recognition not supported');
+    return false;
+  }
+
+
+  var myVids = JSON.parse(vids);
+  var vidContainer = document.getElementsByClassName("vid-container")[0];
+
+  //create video elements from JSON 
+
+  for (var i = 0; i < myVids.length; i++) {
+    var vidEl = document.createElement("video");
+    vidEl.src = "video/" + myVids[i].name + '.mp4';
+    vidEl.classList = "vid " + myVids[i].trigger;
+    vidEl.id = myVids[i].trigger;
+    vidEl.loop = "true ";
+    vidContainer.appendChild(vidEl);
+  }
+
+
+  var recognition = new webkitSpeechRecognition(),
+      recognizing = false,
+      ignoreOnEend,
+      startTimestamp,
+      recognizing,
+      finalTranscript;
+
+  // required DOM nodes
+  var nodes = {
+    start: document.querySelector('[data-start]'),
+    speech: document.querySelector('[data-speech]'),
+    speechInner: document.querySelector('[data-header]'),
+    cube: document.querySelector('[data-vid]')
+  };
+
+  var trigger1 = myVids[0].trigger;
+  // speech triggers and css classes
+  var triggers = {
+    'moon': 'moon',
+    'surface': 'surface',
+    'burger': 'burger',
+    'bottom': 'show-bottom'
+
+  };
+
+  // initialize speech recognition
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.lang = 'en-US';
+
+  // speech recognition started
+  recognition.onstart = function() {
+    recognizing = true;
+    // nodes.start.classList.add('active');
+    // nodes.speech.classList.add('active');
+  };
+
+  // speech recognition end
+  recognition.onend = function(event) {
+    recognizing = false;
+    // nodes.start.classList.remove('active');
+    // nodes.speech.classList.remove('active');
+  };
+
+  // speech recognition result
+  recognition.onresult = function(event) {
+
+    // show speech recognition results
+    // nodes.speech.classList.add('active');
+
+    // loop through results
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+
+      // store transcript and check for match
+      var transcript = event.results[i][0].transcript.trim().toLowerCase(),
+          match = checkForTrigger(transcript);
+
+      // update speech recognition visual
+      nodes.speechInner.innerHTML = transcript;
+
+
+
+      // we have a trigger match
+      if(match) {
+
+        // allow user to see matched trigger with 500ms delay
+        setTimeout(function() {
+
+          // recognizing = false;
+          // recognition.stop();
+
+          // if cube has classes remove all
+          if(nodes.cube.getAttribute('class')) {
+            // DOMTokenList.prototype.remove.apply(nodes.cube.classList, nodes.cube.getAttribute('class').split(' '));
+          }
+
+          // add matched triggers classes
+          // DOMTokenList.prototype.add.apply(nodes.cube.classList, match.class.split(' '));
+      }, 100);
+
+
+        for (var q = 0; q < myVids.length; q++) {
+          var trig = myVids[q].trigger;
+          var vidStop = $( "video" )
+
+          if(match.class == trig) {
+            $("." + trig).fadeIn(300);
+            $("." + trig)[0].play();
+            $( "video" ).not("." + trig).hide();
+
+            $('video').each(function( index ) {
+                if(match.class != this.id){
+                    this.pause();
+                    this.currentTime = 0;
+                }
+            });
+          }
+        }
+      }
+    }
+  };
+
+  // speech recognition error
+  recognition.onerror = function(event) {
+    console.log('Speech Recognition Error');
+    console.log(event);
+    recognition.stop();
+  };
+
+  // record btn - start / stop speech recognition
+  nodes.start.addEventListener('click', function() {
+    if (recognizing) {
+      recognition.stop();
+      recognizing = false;
+      return;
+    }
+
+    recognition.start();
+  });
+
+  // speech visual - stop speech recognition
+  nodes.speech.addEventListener('click', function() {
+    recognition.stop();
+  });
+
+  // accept speech recognition transcript
+  // test for speech recognition trigger match
+  // return object with matched trigger and css classes or false
+  function checkForTrigger(check) {
+    var cls = '',
+        trg = '';
+
+    for(var x in triggers) {
+      var split = x.split(' ');
+
+      split.every(function(s) {
+        if(check.indexOf(s) > -1) {
+          cls = triggers[x];
+          trg = x;
+          return false;
+        }
+
+        return true;
+      });
+    }
+
+    return cls && trg ? {
+      class: cls,
+      trigger: trg
+    } : false;
+  }
+
+  // instantiate material design modal JS
+  $(document).ready(function() {
+    // $('.modal-trigger').leanModal();
+  });
+})();
